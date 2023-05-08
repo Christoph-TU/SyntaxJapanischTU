2bf14675a28ec70e8f092f31f5c4afc4686c4067
Christoph Weber <e11908087@student.tuwien.ac.at>
2023-05-08 17:36:45 +0200

    setup

---
+++ generate_dictionary.py
@@ -0,0 +1,34 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+import subprocess
+
+# Load JSON data
+with open("entries.json", "r", encoding="utf-8") as f:
+    entries = json.load(f)
+
+# Sort entries based on the 'name' attribute
+sorted_entries = sorted(entries, key=lambda x: x['name'])
+
+# Overwrite the original JSON file with the sorted entries
+with open("entries.json", "w", encoding="utf-8") as f:
+    json.dump(sorted_entries, f, ensure_ascii=False, indent=2)
+
+# Set up Jinja2 environment
+env = Environment(loader=FileSystemLoader("."), trim_blocks=True, lstrip_blocks=True)
+template = env.get_template("template.tex")
+
+# Render LaTeX template with JSON data
+output_tex = template.render(entries=sorted_entries)
+
+# Write the output to a .tex file
+with open("dictionary_output.tex", "w", encoding="utf-8") as f:
+    f.write(output_tex)
+
+try:
+    subprocess.run(["pdflatex", "dictionary_output.tex"], check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error during the execution of pdflatex: {e}")
+    print("Please ensure that pdflatex is installed and available in your system's PATH.")
+except FileNotFoundError as e:
+    print(f"Error: {e}")
+    print("pdflatex not found. Please ensure that pdflatex is installed and available in your system's PATH.")
